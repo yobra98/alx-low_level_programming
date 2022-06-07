192c2d5257b4809b4c2d5fb75ee4a4b9366e359c
brian yobra <briannjoroge89@gmail.com>
2022-06-07 10:37:16 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,4 @@
-#include "holberton.h"
-
-/**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
- */
+#include "main.h"
 void jack_bauer(void)
 {
 	int i, j;
