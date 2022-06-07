df52b444c3fc25cdcd4a3a849a215adfafff09dc
brian yobra <briannjoroge89@gmail.com>
2022-06-07 10:36:04 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,4 @@
-#include "holberton.h"
-
-/**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
- */
+#include "main.h"
 int print_sign(int n)
 {
 	if (n > 0)
