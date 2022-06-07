c2ca2f6669d2d99d691e975f8f0e592c4f5b70df
brian yobra <briannjoroge89@gmail.com>
2022-06-07 10:38:17 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,4 @@
-#include "holberton.h"
-
-/**
- * add - adds two integers and returns the result
- * @a: int to be added to b
- * @b: int to be added to a
- * Return: sum of a and b
- */
+#include "main.h"
 int add(int a, int b)
 {
 	int c;
